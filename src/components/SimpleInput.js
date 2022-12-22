fadc8a1ff32836b8855d473dcb39b9fd9c64e5a0
Payal Sasmal <sasmalpayal@gmail.com>
2022-12-22 15:57:22 +0530

    toucched input using simple input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,25 +2,42 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameValid, setEnteredNameValid] = useState(true);
+  const [enteredNameValid, setEnteredNameValid] = useState(false);
+  const [enteredNameTouchedValid, setEnteredNameTouchedValid] = useState(false);
 
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formChangeHandler = (event) => {
-    event.preventDefault();
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouchedValid(true);
     
     if(enteredName.trim() === ''){
       setEnteredNameValid(false);
       return;
     }
+  };
+
+  const formChangeHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouchedValid(true);
+    
+    if(enteredName.trim() === ''){
+      setEnteredNameValid(false);
+      return;
+    }
+    
+    setEnteredNameValid(true);
+    
     console.log(enteredName);
     setEnteredName("");
   };
 
-  const nameInputClassName = enteredNameValid ? 'form-control' : 'form-control invalid';
+  const nameInputNameInvalid = !enteredNameValid && enteredNameTouchedValid;
+
+  const nameInputClassName = nameInputNameInvalid ? 'form-control' : 'form-control invalid';
 
   return (
     <form onSubmit={formChangeHandler}>
@@ -30,9 +47,10 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
-        {!enteredNameValid && <p className={"error-text"}>Name must not be empty.</p>}
+        {nameInputNameInvalid && <p className={"error-text"}>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
